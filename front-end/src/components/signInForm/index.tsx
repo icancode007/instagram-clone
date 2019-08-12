1ac11f0f7cc008ff892a8a896d862991ca70b2c8
Jeffrey-A <almanzarortizjeffrey@gmail.com>
2019-08-11 23:33:51 -0400

    fix(instagram-clone): remove extra spaces  task-4

--- front-end/src/components/signInForm/index.tsx
+++ front-end/src/components/signInForm/index.tsx
@@ -7,7 +7,7 @@ interface State {
 }
 
 interface Event {
-    target: { value: string}
+    target: { value: string }
 }
 
 class SignInForm extends React.Component<Object, State>{
@@ -18,15 +18,14 @@ class SignInForm extends React.Component<Object, State>{
             username: "",
             password: ""
         }
-
     }
 
-    handleUserNameChange = (e:Event)=>{
-        this.setState({username: e.target.value})
+    handleUserNameChange = (e: Event) => {
+        this.setState({ username: e.target.value })
     }
 
-    handlePasswordChange = (e: Event)=>{
-        this.setState({password: e.target.value})
+    handlePasswordChange = (e: Event) => {
+        this.setState({ password: e.target.value })
     }
 
     render() {
@@ -35,21 +34,21 @@ class SignInForm extends React.Component<Object, State>{
                 <h1>Instagram</h1>
                 <div>
                     <form>
-                        <div className= "input-container">
-                            <input type="text" placeholder="Phone number, username, or email" value={this.state.username} 
-                            onChange={this.handleUserNameChange} />
+                        <div className="input-container">
+                            <input type="text" placeholder="Phone number, username, or email" value={this.state.username}
+                                onChange={this.handleUserNameChange} />
                         </div>
 
-                        <div className= "input-container">
+                        <div className="input-container">
                             <input type="password" placeholder="Password" value={this.state.password}
-                            onChange={this.handlePasswordChange}
+                                onChange={this.handlePasswordChange}
                             />
                         </div>
 
-                       <div className= "input-container">
-                         <input type="submit" />
-                       </div>
-                        
+                        <div className="input-container">
+                            <input type="submit" />
+                        </div>
+
                     </form>
                 </div>
 
