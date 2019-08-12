91008acc437855588b28a55030da30655bedb959
Jeffrey-A <almanzarortizjeffrey@gmail.com>
2019-08-11 23:28:43 -0400

    feat(instagram-clone): create basic sign-in component task-4

---
+++ front-end/src/components/signInForm/index.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import './style.scss';
+
+interface State {
+    username: string,
+    password: string
+}
+
+interface Event {
+    target: { value: string}
+}
+
+class SignInForm extends React.Component<Object, State>{
+    constructor(props: Object) {
+        super(props);
+
+        this.state = {
+            username: "",
+            password: ""
+        }
+
+    }
+
+    handleUserNameChange = (e:Event)=>{
+        this.setState({username: e.target.value})
+    }
+
+    handlePasswordChange = (e: Event)=>{
+        this.setState({password: e.target.value})
+    }
+
+    render() {
+        return (
+            <div className="form-container">
+                <h1>Instagram</h1>
+                <div>
+                    <form>
+                        <div className= "input-container">
+                            <input type="text" placeholder="Phone number, username, or email" value={this.state.username} 
+                            onChange={this.handleUserNameChange} />
+                        </div>
+
+                        <div className= "input-container">
+                            <input type="password" placeholder="Password" value={this.state.password}
+                            onChange={this.handlePasswordChange}
+                            />
+                        </div>
+
+                       <div className= "input-container">
+                         <input type="submit" />
+                       </div>
+                        
+                    </form>
+                </div>
+
+            </div>
+        )
+    }
+
+}
+
+export default SignInForm;
