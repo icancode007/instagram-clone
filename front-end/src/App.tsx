bb5829705d2024e318521eafcf0eb70d9b26f24c
Jeffrey-A <almanzarortizjeffrey@gmail.com>
2019-08-12 23:18:03 -0400

    fix(instagram-clone): fix sign-in form, create sign-up form and delete unnecessary files task-2-1

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import UserForm from './components/UserForm';
-import SignInForm from './components/signInForm';
+
 
 import './App.scss';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <SignInForm />
+      <UserForm />
     </div>
   );
 }
--- front-end/src/components/UserForm/index.tsx
+++ front-end/src/components/UserForm/index.tsx
@@ -1,17 +1,79 @@
 import * as React from 'react';
+import './style.scss';
 
 export default class Counter extends React.Component {
   //could be refactored to functional component if hooks will be used
-  componentDidMount(){
+  componentDidMount() {
     fetch('/root')
-    .then(res => res.json())
-    .then(data => console.log(data));
+      .then(res => res.json())
+      .then(data => console.log(data));
   }
 
-  render () {
+  render() {
+    // Rendering both forms for now. 
+    // We can also create them in two different files and render them here. 
     return (
       <div>
-      testing
+        {/* Log In */}
+        <div className="form-container">
+          <h1>Instagram</h1>
+          <div>
+            <form>
+              <div className="input-container">
+                <input type="text" placeholder="Phone number, username, or email" />
+              </div>
+
+              <div className="input-container">
+                <input type="password" placeholder="Password" />
+              </div>
+
+              <div className="submit-container">
+                <input type="submit" className="submit-button" value="Log In" />
+              </div>
+
+            </form>
+          </div>
+
+        </div>
+
+        {/*  Sign up */}
+        <div className="form-container">
+          <h1>Instagram</h1>
+          <div className="sign-up-top-paragraph-container">
+            <p>Sign up to see photos and videos from your friends.</p>
+          </div>
+
+          <div>
+            <form>
+              <div className="input-container">
+                <input type="text" placeholder="Mobile number or email" />
+              </div>
+
+              <div className="input-container">
+                <input type="text" placeholder="Full Name" />
+              </div>
+
+              <div className="input-container">
+                <input type="text" placeholder="Username" />
+              </div>
+
+              <div className="input-container">
+                <input type="password" placeholder="Password" />
+              </div>
+
+              <div className="submit-container">
+                <input type="submit" className="submit-button" value="Sign Up" />
+              </div>
+
+              <div className="sign-up-bottom-paragraph-container">
+                <p>By signing up, you agree to our <span>Terms</span> , <span>Data Policy</span> and <span>Cookies Policy</span>.</p>
+              </div>
+
+            </form>
+          </div>
+
+        </div>
+
       </div>
     );
   }
