93654a9284308e89eecad6c1ccf4fc2ba333c3d5
Jeffrey-A <almanzarortizjeffrey@gmail.com>
2019-08-17 22:11:47 -0400

    fix(instagram-clone): add error checking to sign up form  task-2-1

--- front-end/src/components/UserForm/SignIn.tsx
+++ front-end/src/components/UserForm/SignIn.tsx
@@ -4,29 +4,38 @@ interface UserForm {
     toggleUserForm: any
 }
 
-const SignIn = ({ toggleUserForm }: UserForm): JSX.Element => (
-    <div>
-        <div className="form-container">
-            <h1>Instagram</h1>
+class SignIn extends React.Component<UserForm>{
+
+    submit = (e: React.FormEvent): void => {
+        e.preventDefault();
+    }
+
+    render() {
+        const { toggleUserForm } = this.props;
+        return (
             <div>
-                <form>
-                    <div className="input-container">
-                        <input type="text" placeholder="Phone number, username, or email" />
+                <div className="form-container">
+                    <h1>Instagram</h1>
+                    <div>
+                        <form onSubmit={this.submit}>
+                            <div className="input-container">
+                                <input type="text" placeholder="Phone number, username, or email" />
+                            </div>
+                            <div className="input-container">
+                                <input type="password" placeholder="Password" />
+                            </div>
+                            <div className="submit-container">
+                                <input type="submit" className="submit-button" value="Log In" />
+                            </div>
+                        </form>
                     </div>
-                    <div className="input-container">
-                        <input type="password" placeholder="Password" />
-                    </div>
-                    <div className="submit-container">
-                        <input type="submit" className="submit-button" value="Log In" />
-                    </div>
-                </form>
-            </div>
-        </div>
-        <div className="form-container">
-            <p>Don't have an account? <button onClick={toggleUserForm} className="lg-footer-sign-up-btn">Sign up</button></p>
-        </div>
-    </div>
-);
+                </div>
+                <div className="form-container">
+                    <p>Don't have an account? <button onClick={toggleUserForm} className="lg-footer-sign-up-btn">Sign up</button></p>
+                </div>
+            </div>);
+    }
+}
 
 export default SignIn;
 
--- front-end/src/components/UserForm/SignUp.tsx
+++ front-end/src/components/UserForm/SignUp.tsx
@@ -1,43 +1,115 @@
 import React from 'react';
-
+import errorImg from './assets/error.png';
+// TODO: move the state to the parent component.
 interface UserForm {
   toggleUserForm: any
 }
 
-const SignUp = ({ toggleUserForm }: UserForm): JSX.Element => (
-  <div>
-    <div className="form-container">
-      <h1>Instagram</h1>
-      <div className="sign-up-top-paragraph-container">
-        <p>Sign up to see photos and videos from your friends.</p>
-      </div>
+interface State {
+  errors: string[],
+  numberOfErrors: number
+}
+
+class SignUp extends React.Component<UserForm, State>{
+  state = {
+    errors: [''],
+    numberOfErrors: 0
+
+  }
+
+  submit = (e: any): void => {
+    e.preventDefault();
+    let inputChildren = document.querySelectorAll('.sign-up-input');
+    let errors: string[] = [];
+
+
+    for (let i = 0; i < inputChildren.length; i++) {
+      const input: any = inputChildren[i];
+
+      if (!input.value) {
+
+        input.onfocus = () => {
+          this.setState((prevState) => {
+            const updatedNumberOfErrors = prevState.numberOfErrors - 1;
+            const updatedErrors = prevState.errors;
+            updatedErrors[i] = "";
+            return { errors: updatedErrors, numberOfErrors: updatedNumberOfErrors }
+          });
+        }
+
+        errors[i] = `input-${i}`;
+      }
+    }
+
+    if (errors.length) {
+      this.setState({ errors, numberOfErrors: errors.length })
+    }
+  }
+
+  renderInputWithError = (searchTerm: string): boolean => {
+    const { errors } = this.state;
+    return errors.includes(searchTerm);
+  }
+
+  displayErrorMessage = () => {
+    const { numberOfErrors } = this.state;
+    if (numberOfErrors > 1) {
+      return 'These fields are required.'
+    } else if (numberOfErrors === 1) {
+      return "This field is required.";
+    } else {
+      return null;
+    }
+  }
+
+  render() {
+    const { toggleUserForm } = this.props;
+    return (
       <div>
-        <form>
-          <div className="input-container">
-            <input type="text" placeholder="Mobile number or email" />
+        <div className="form-container">
+          <h1>Instagram</h1>
+          <div className="sign-up-top-paragraph-container">
+            <p>Sign up to see photos and videos from your friends.</p>
           </div>
-          <div className="input-container">
-            <input type="text" placeholder="Full Name" />
+          <div>
+            <form onSubmit={this.submit} >
+              <div className="input-container">
+                <input type="text" className="sign-up-input" placeholder="Mobile number or email" />
+                {this.renderInputWithError('input-0') ? <img className="error-image" src={errorImg} alt="error" /> : null}
+              </div>
+              <div className="input-container">
+                <input type="text" className="sign-up-input" placeholder="Full Name" />
+                {this.renderInputWithError('input-1') ? <img className="error-image" src={errorImg} alt="error" /> : null}
+              </div>
+              <div className="input-container">
+                <input type="text" className="sign-up-input" placeholder="Username" />
+                {this.renderInputWithError('input-2') ? <img className="error-image" src={errorImg} alt="error" /> : null}
+              </div>
+              <div className="input-container">
+                <input type="text" className="sign-up-input" placeholder="Password" />
+                {this.renderInputWithError('input-3') ? <img className="error-image" src={errorImg} alt="error" /> : null}
+              </div>
+              <div className="submit-container">
+                <input type="submit" className="submit-button" value="Sign Up" />
+              </div>
+              <div className="error-container">
+                {
+                  this.displayErrorMessage()
+
+                }
+              </div>
+              <div className="sign-up-bottom-paragraph-container">
+                <p>By signing up, you agree to our <span>Terms</span> , <span>Data Policy</span> and <span>Cookies Policy</span>.</p>
+              </div>
+            </form>
           </div>
-          <div className="input-container">
-            <input type="text" placeholder="Username" />
-          </div>
-          <div className="input-container">
-            <input type="password" placeholder="Password" />
-          </div>
-          <div className="submit-container">
-            <input type="submit" className="submit-button" value="Sign Up" />
-          </div>
-          <div className="sign-up-bottom-paragraph-container">
-            <p>By signing up, you agree to our <span>Terms</span> , <span>Data Policy</span> and <span>Cookies Policy</span>.</p>
-          </div>
-        </form>
+        </div>
+        <div className="form-container">
+          <p>Have an account? <button onClick={() => toggleUserForm('signUp')} className="lg-footer-sign-up-btn">Log In</button></p>
+        </div>
       </div>
-    </div>
-    <div className="form-container">
-      <p>Have an account? <button onClick={() => toggleUserForm('signUp')} className="lg-footer-sign-up-btn">Log In</button></p>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default SignUp;
