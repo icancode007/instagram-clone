50b8db1c16cc6a6e6de7887909c3a478059a9b08
Jose Franny Rodriguez <icancode911@gmail.com>
2019-10-09 23:54:54 -0400

    refactor(instagram-clone): revisit input validations, currently not behaving as expected

--- front-end/src/components/UserForm/SignIn.tsx
+++ front-end/src/components/UserForm/SignIn.tsx
@@ -12,10 +12,10 @@ export interface State {
 class SignIn extends React.Component<UserFormProps, State>{
     state = {
         isSubmitButtonEnable: false,
-        username: "",
+        username: '',
         showBtnInPasswordInput: false,
         isShowingPassword: false,
-        error: ""
+        error: ''
     }
 
     handleUserNameChange = (event: { target: { value: string; }; }): void => {
@@ -24,26 +24,23 @@ class SignIn extends React.Component<UserFormProps, State>{
 
     submit = async (event: React.FormEvent): Promise<void> => {
         event.preventDefault();
-        const phoneNumberOrEmail: any = document.getElementById('email-phone-username');
+        const logInHandle: any = document.getElementById('email-phone-username');
         const passwordInput: any = document.getElementById("password");
-        const { isValidPhoneNumber, isValidEmail, isValidUserName, isValidPassword }= this.props;
+        const { isValidPassword }= this.props;
 
-        const isValidInput = isValidEmail(phoneNumberOrEmail.value)
-        || isValidPhoneNumber(phoneNumberOrEmail.value)
-        || isValidUserName(phoneNumberOrEmail.value);
-
-       if (isValidInput && isValidPassword(passwordInput.value)) {
+       if (isValidPassword(passwordInput.value)) {
           const postSettings = {
             method: 'POST',
             body: JSON.stringify({
-                username: phoneNumberOrEmail,
+                username: logInHandle,
                 password: passwordInput
               })
             }
-          await fetch('/login', postSettings);
+          const data = await fetch('/login', postSettings);
+        
         } else {
             // checks where is the error
-            if (isValidInput) {
+            if (logInHandle) {
                 // Then the password is invalid
                 this.setState({ error: "password" })
             } else {
@@ -72,44 +69,58 @@ class SignIn extends React.Component<UserFormProps, State>{
         }
     }
 
-    render() {
-        const { toggleUserForm } = this.props;
-        const { username, isShowingPassword, showBtnInPasswordInput, error } = this.state;
-        let errorMessage;
+  render() {
+      const { toggleUserForm } = this.props;
+      const { username, isShowingPassword, showBtnInPasswordInput, error } = this.state;
+      let errorMessage;
 
-        if (error === "username") {
-            errorMessage = <div className="error-container"><span>The username you entered doesn't belong to an account.</span></div>;
-        } else if (error === "password") {
-            errorMessage = <div className="error-container"><span>Sorry, your password was incorrect.</span></div>;
-        }
+      if (error === "username") {
+          errorMessage = <div className="error-container"><span>The username you entered doesn't belong to an account.</span></div>;
+      } else if (error === "password") {
+          errorMessage = <div className="error-container"><span>Sorry, your password was incorrect.</span></div>;
+      }
 
-        return (
-            <div>
-                <div className="form-container sign-in">
-                    <h1>Instagram</h1>
-                    <div>
-                        <form onSubmit={this.submit}>
-                            <div className="input-container">
-                                <input id="email-phone-username" type="text" placeholder="Phone number, username, or email" onChange={this.handleUserNameChange} value={username} />
-                            </div>
-                            <div className="input-container">
-                                <input id="password" type="password" placeholder="Password" onChange={this.handlePasswordChange} />
-                                {showBtnInPasswordInput ? <button className="show-hide-btn" onClick={this.togglePassword}> {isShowingPassword ? "Hide" : "Show"}</button> : null}
-                            </div>
-                            <div className="submit-container">
-                                {username
-                                    ? <input type="submit" className="submit-button" value="Log In" />
-                                    : <input type="submit" className="submit-button-disable" value="Log In" disabled />
-                                }
-                            </div>
-                            {errorMessage}
-                        </form>
-                    </div>
-                </div>
-                <div className="form-container">
-                    <p>Don't have an account? <button onClick={toggleUserForm} className="footer-btn">Sign up</button></p>
-                </div>
-            </div>);
+      return (
+        <>
+          <div className="form-container sign-in">
+              <h1>Instagram</h1>
+              <form onSubmit={this.submit}>
+                  <div className="input-container">
+                      <input
+                        id="email-phone-username"
+                        type="text"
+                        placeholder="Phone number, username, or email"
+                        onChange={this.handleUserNameChange}
+                        value={username}
+                      />
+                  </div>
+                  <div className="input-container">
+                      <input
+                        id="password"
+                        type="password"
+                        placeholder="Password"
+                        onChange={this.handlePasswordChange}
+                      />
+                      {
+                        showBtnInPasswordInput
+                        ? <button className="show-hide-btn" onClick={this.togglePassword}> {isShowingPassword ? "Hide" : "Show"}</button> :
+                         null
+                       }
+                  </div>
+                  <div className="submit-container">
+                      {username
+                          ? <input type="submit" className="submit-button" value="Log In" />
+                          : <input type="submit" className="submit-button-disable" value="Log In" disabled />
+                      }
+                  </div>
+                  {errorMessage}
+              </form>
+          </div>
+          <div className="form-container">
+              <p>Don't have an account? <button onClick={toggleUserForm} className="footer-btn">Sign up</button></p>
+          </div>
+        </>
+      );
     }
 }
 
